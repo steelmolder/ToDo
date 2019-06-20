975c0d2ed375c87e8ded017bfd3f86140aac6656
steelmolder <steelmolder@gmail.com>
2019-06-20 15:40:50 +0300

    first commit

---
+++ ToDo.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ToDo {
+
+    public static void main (String[] args) {
+        ArrayList <String> business = new ArrayList<String>();
+        business.add("Clean room");
+        business.add("Go to school");
+        business.add("eat and relax");
+        business.add("to go home");
+        business.add("eat and go slleep");
+        System.out.println("all yours affairs : " + business.size()+ business );
+    }
+}
